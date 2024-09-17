03b2fbc1600834629fe1f91f7715f87485540aa8
Samir Sheldenkar <github@sheldenkar.co.uk>
2024-09-17 16:15:42 +0100

    Update comments for vol calcs

--- sysquant/estimators/vol.py
+++ sysquant/estimators/vol.py
@@ -31,8 +31,8 @@ def robust_vol_calc(
     We apply an absolute minimum level of vol (absmin);
     and a volfloor based on lowest vol over recent history
 
-    :param x: data
-    :type x: Tx1 pd.Series
+    :param daily_returns: data
+    :type daily_returns: pd.Series
 
     :param days: Number of days in lookback (*default* 35)
     :type days: int
@@ -58,6 +58,10 @@ def robust_vol_calc(
     :param floor_min_periods: Minimum observations for floor - until reached
       floor is zero (*default* 100)
     :type floor_min_periods: int
+
+    :param backfill: Uses the first calculated vol for initial period 
+      (note this therrefore has some forward looking bias)
+    :type backfill: bool
 
     :returns: pd.DataFrame -- volatility measure
     """
@@ -129,12 +133,13 @@ def mixed_vol_calc(
     **ignored_kwargs,
 ) -> pd.Series:
     """
-    Robust exponential volatility calculation, assuming daily series of prices
+    Mixed exponential volatility calculation, assuming daily series of prices
+    Applies a proportion to long-term (slow vol) based on N years lookback 
     We apply an absolute minimum level of vol (absmin);
-    and a volfloor based on lowest vol over recent history
+    Note however implementation does not apply a separate volfloor based on lowest vol over recent history
 
-    :param x: data
-    :type x: Tx1 pd.Series
+    :param daily_returns: data
+    :type daily_returns: pd.Series
 
     :param days: Number of days in lookback (*default* 35)
     :type days: int
@@ -146,20 +151,9 @@ def mixed_vol_calc(
       0.0= not used
     :type absmin: float or None
 
-    :param vol_floor Apply a floor to volatility (*default* True)
-    :type vol_floor: bool
-
-    :param floor_min_quant: The quantile to use for volatility floor (eg 0.05
-      means we use 5% vol) (*default 0.05)
-    :type floor_min_quant: float
-
-    :param floor_days: The lookback for calculating volatility floor, in days
-      (*default* 500)
-    :type floor_days: int
-
-    :param floor_min_periods: Minimum observations for floor - until reached
-      floor is zero (*default* 100)
-    :type floor_min_periods: int
+    :param backfill: Uses the first calculated vol for initial period 
+      (note this therrefore has some forward looking bias)
+    :type backfill: bool
 
     :returns: pd.DataFrame -- volatility measure
     """
